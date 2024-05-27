d5e4b162cfc98cb1a89196ab2e89ce9661d07b68
gam17 <gam17@users.noreply.github.com>
2024-05-27 15:49:44 +0200

    trad

---
+++ help/lrelease.bat
@@ -0,0 +1,7 @@
+rem lrelease is a command line tool that produces QM files out of TS files.
+
+rem prima avviare OSGeo4W Shell e da lì:
+rem cd "%AppData%\QGIS\QGIS3\profiles\default\python\plugins\qad\i18n"
+rem lrelease
+"C:\Program Files\QGIS 3.22.6\Library\bin\lrelease.exe" Qad.pro
+pause
